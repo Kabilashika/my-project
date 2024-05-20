ac7ca66b3db8437dd82b2a3bab8b899663d5c10a
Kabilashika <thiyagarajahkabilashika@gmail.com>
2024-05-20 16:22:52 +0530

    animal food created

---
+++ animals.py
@@ -0,0 +1,14 @@
+def animal_info():
+    print("\nAnimal Information:")
+    print("Species: Dog")
+    print("Breed: Golden Retriever")
+    print("Age: 5")
+    print("Name: Gimmy")
+
+def favorite_food():
+    print("\nFavorite Food:")
+    print("Gimmy loves to eat chicken.")
+
+if __name__ == "__main__":
+    animal_info()
+    favorite_food()
---
+++ car.py
@@ -0,0 +1,9 @@
+def car_info():
+    print("\nCar Information:")
+    print("Make: Toyota")
+    print("Model: Corolla")
+    print("Year: 2020")
+    print("Color: Red")
+
+if __name__ == "__main__":
+    car_info()
